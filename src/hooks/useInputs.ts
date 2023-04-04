7acec673bc3218adb907677d597a887cef823656
Jung Su <wjdwjdtn92@naver.com>
2023-04-04 16:38:22 +0900

    [Style] Add arrow function return eslint 수정에 따른 변경사항 적용

--- src/hooks/useInputs.ts
+++ src/hooks/useInputs.ts
@@ -2,12 +2,16 @@ import React, { useState, useCallback } from 'react';
 
 function useInputs<T extends Record<string, any>>(initialForm: T) {
   const [form, setForm] = useState<T>(initialForm);
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  const reset = useCallback(() => {
+    setForm(initialForm);
+  }, [initialForm]);
 
   const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
-    setForm((preState) => ({ ...preState, [name]: value }));
+    setForm((preState) => {
+      return { ...preState, [name]: value };
+    });
   }, []);
 
   return [form, onChange, reset] as const;
