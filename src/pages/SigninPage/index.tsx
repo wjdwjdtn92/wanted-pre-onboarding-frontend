f8578a0bb5769767a819bd4d4e9b1353627b25de
Jung Su <wjdwjdtn92@naver.com>
2023-04-05 21:28:32 +0900

    [Feat] 로그인 기능 구현 로그인 로직 구현,  성공시 todo로 리다이렉트 및 로컬스토리지에 저장

--- src/pages/SigninPage/index.tsx
+++ src/pages/SigninPage/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthCredentials } from '../../@type/auth.type';
+import useAuth from '../../Auth/useAuth';
+import AuthForm from '../../components/AuthForm';
+import { signin } from '../../utils/api/authApi';
+import styles from './SignIn.module.scss';
 
-function SigninPage() {
-  return <div>SigninPage</div>;
+function SignupPage() {
+  const navigate = useNavigate();
+  const { setAccessToken } = useAuth();
+
+  const handleSubmit = async ({ email, password }: AuthCredentials) => {
+    try {
+      const response = await signin({ email, password });
+      console.log(response);
+      const accessToken = response.access_token;
+
+      if (accessToken?.length === 0) {
+        console.log('실패');
+        return;
+      }
+
+      localStorage.setItem('access_token', accessToken);
+      setAccessToken(accessToken);
+      navigate('/todo');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <main className={styles.main}>
+      <AuthForm authType="signin" onSubmit={handleSubmit} />
+    </main>
+  );
 }
 
-export default SigninPage;
+export default SignupPage;
