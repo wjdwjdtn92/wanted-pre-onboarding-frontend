b5bad2a421ab8aeb1e0daf5e338670271e39e1d9
Jung Su <wjdwjdtn92@naver.com>
2023-04-12 09:09:16 +0900

    [Feat] handleSubmit, handleChecked 기능 구현 수정시 서버전송을 위해 이벤트 처리 추가

--- src/components/Todos/TodoItem/index.tsx
+++ src/components/Todos/TodoItem/index.tsx
@@ -1,48 +1,85 @@
 import React, { useState } from 'react';
-import { TodoType } from '../../../@type/todo.type';
+import { TodoUpdateType, TodoType } from '../../../@type/todo.type';
 import Button from '../../UI/Button';
+import Input from '../../UI/Input';
 import styles from './TodoItem.module.scss';
 
-type TodoItemProps = TodoType & {
-  onEdit: ({ id }: { id: number }) => void;
+export type TodoItemProps = TodoType & {
+  onUpdate: ({ id, isCompleted, todo }: TodoUpdateType) => Promise<void>;
   onDelete: ({ id }: { id: number }) => void;
 };
 
-function TodoItem({ id, todo, isCompleted, onEdit, onDelete }: TodoItemProps) {
+function TodoItem({ id, todo, isCompleted, onUpdate, onDelete }: TodoItemProps) {
   const [isChecked, setIsChecked] = useState(isCompleted);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editTodo, setEditTodo] = useState(todo);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newTodo = event.target.value;
+    setEditTodo(newTodo);
+  };
+
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+
+  const handleDelete = () => {
+    onDelete({ id });
+  };
+
+  const handleSubmit = () => {
+    onUpdate({ id, todo: editTodo, isCompleted: isChecked });
+    setIsEdit(false);
+  };
+
+  const handleCancle = () => {
+    setEditTodo(todo);
+    setIsEdit(false);
+  };
 
   const handleChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(event.target.checked);
-  };
+    const { checked } = event.target;
+    onUpdate({ id, todo: editTodo, isCompleted: !checked });
 
-  const handleEditButtonClick = () => {
-    onEdit({ id });
-  };
-
-  const handleDeletetButtonClick = () => {
-    onDelete({ id });
+    setIsChecked((preSate) => {
+      return !preSate;
+    });
   };
 
   return (
     <li className={styles.todo} data-todo-id={id}>
-      <label className={styles.todo_label} htmlFor="todo-checkbox">
-        <input id="todo-checkbox" type="checkbox" checked={isChecked} onChange={handleChecked} />
-        <span>{todo}</span>
+      <label className={styles.todo_label} htmlFor={`todo-checkbox-${id}`}>
+        <input
+          id={`todo-checkbox-${id}`}
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleChecked}
+        />
+        {isEdit ? (
+          <Input name="content" type="text" value={editTodo} onChange={handleChange} />
+        ) : (
+          <span>{todo}</span>
+        )}
       </label>
-      <Button
-        className={styles.todo_button}
-        onClick={handleEditButtonClick}
-        data-testid="modify-button"
-      >
-        수정
-      </Button>
-      <Button
-        className={styles.todo_button}
-        onClick={handleDeletetButtonClick}
-        data-testid="delete-button"
-      >
-        삭제
-      </Button>
+      {isEdit ? (
+        <>
+          <Button className={styles.todo_button} onClick={handleSubmit} data-testid="submit-button">
+            제출
+          </Button>
+          <Button className={styles.todo_button} onClick={handleCancle} data-testid="cancel-button">
+            취소
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button className={styles.todo_button} onClick={handleEdit} data-testid="modify-button">
+            수정
+          </Button>
+          <Button className={styles.todo_button} onClick={handleDelete} data-testid="delete-button">
+            삭제
+          </Button>
+        </>
+      )}
     </li>
   );
 }
